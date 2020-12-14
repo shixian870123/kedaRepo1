f071d8bd2f2c6059ef4ba51e7ced2aa65fb58c0b
shixian <13521348721@163.com>
2020-12-14 10:00:09 +0800

    第二次修改 HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld{
 	public static void main(String[] agrs){
 		System.out.println("Hello World!!!");
 		System.out.println("Ha ha!!!");
+		System.out.println("Xi xi!!!");
 	}
 }
